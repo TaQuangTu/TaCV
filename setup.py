2014104a6a62e1177d52b188632db5e1faeff64f
tu <taquangtu132@gmail.com>
2022-02-09 17:37:57 +0700

    Add setup.py and test.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tacv',
+    version='1.0.0',
+    packages=['tacv', 'tacv.visual', 'tacv.fileUtils'],
+    url='github.com/TaQuangTu',
+    license='',
+    author='tutq',
+    author_email='taquangtu132@gmail.com',
+    description='A mini package for daily tasks'
+)
---
+++ test.py
@@ -0,0 +1,9 @@
+
+
+
+
+from tacv.fileUtils.fileutils import get_all_files
+
+if __name__ == "__main__":
+    file_list = get_all_files("../tupkgs")
+    debug = 0
