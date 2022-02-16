10fb47bd69bcf8eeae7b8c860be3276623ebe097
taquangtu <taquangtu132@gmail.com>
2022-02-16 17:35:07 +0700

    Add license and update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 import setuptools
+
+with open("README.md","r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='tacv',
-    version='1.0.0',
+    version='1.0.1',
     packages= setuptools.find_packages(where="."),
     url='https://github.com/TaQuangTu/TaCV',
     license='',
     author='TaQuangTu',
     author_email='taquangtu132@gmail.com',
     description='A mini package for daily tasks',
+    long_description_content_type = "text/markdown",
+    long_description=long_description,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
