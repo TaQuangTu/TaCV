ab6d5e11a99d7eebed48605113ca42e44470fd2b
taquangtu <taquangtu132@gmail.com>
2022-02-21 15:32:54 +0700

    Update version Add requirements when install

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ import setuptools
 
 with open("README.md","r",encoding="utf-8") as fh:
     long_description = fh.read()
-
+with open("requirements.txt","r") as f:
+    lines = f.readlines()
+    required_pkgs = [item.strip() for item in lines]
 setup(
     name='tacv',
-    version='1.0.2',
+    version='1.0.6',
     packages= setuptools.find_packages(where="."),
     url='https://github.com/TaQuangTu/TaCV',
-    license='',
+    license='LICENSE',
     author='TaQuangTu',
+    install_requires = required_pkgs,
     author_email='taquangtu132@gmail.com',
     description='A mini package for daily tasks',
     long_description_content_type = "text/markdown",
