d97b9ce175f4b23fcb4fe9ac5751810fde634912
tu <taquangtu132@gmail.com>
2022-06-02 17:06:04 +0700

    Add version update automatically in build step

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 rm -rf dist
+rm -rf tacv.egg-info
+rm -rf build
 python -m build
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,22 +1,53 @@
 from setuptools import setup
 import setuptools
+import os
 
-with open("README.md","r",encoding="utf-8") as fh:
+def read_version(version_file_path):
+    if not os.path.exists(version_file_path):
+        return None
+    with open(version_file_path,"r") as version_file:
+        text = version_file.readlines()[0]
+        version_file.close()
+        batches = text.split(".")
+        batches = [int(batch) for batch in batches]
+        return batches
+
+def increase_version_by_one(version_file_path):
+    batches = read_version(version_file_path)
+    if batches is None:
+        return
+    batch1, batch2, batch3 = batches
+    batch3 += 1
+    if batch3 >= 10:
+        batch3 = batch3 % 10
+        batch2 += 1
+        if batch2 >= 10:
+            batch1 = batch1 + 1
+    version_file = open(version_file_path,"w+")
+    new_ver = str(batch1)+"."+str(batch2)+"."+str(batch3)
+    version_file.write(new_ver+"\n")
+    version_file.close()
+
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-with open("requirements.txt","r") as f:
+with open("requirements.txt", "r") as f:
     lines = f.readlines()
     required_pkgs = [item.strip() for item in lines]
+version_file = "tacv/resources/version.txt"
+increase_version_by_one(version_file)
+new_ver = read_version(version_file)
+print(f"Building {new_ver}")
 setup(
     name='tacv',
-    version='1.0.9',
-    packages= setuptools.find_packages(where="."),
+    version=new_ver,
+    packages=setuptools.find_packages(where="."),
     url='https://github.com/TaQuangTu/TaCV',
     license='LICENSE',
     author='TaQuangTu',
-    install_requires = required_pkgs,
+    install_requires=required_pkgs,
     author_email='taquangtu132@gmail.com',
     description='A mini package for daily tasks',
-    long_description_content_type = "text/markdown",
+    long_description_content_type="text/markdown",
     long_description=long_description,
     classifiers=[
         "Programming Language :: Python :: 3",
